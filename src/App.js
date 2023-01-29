121e3609a2042a34f00a7c7c27f30bf7568be2d1
Dmitry <brepex@21vek.it>
2023-01-30 00:56:29 +0700

    Merge remote-tracking branch 'origin/main'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 function App() {
 
-  console.log(window.Telegram.WebApp)
+  const tg = window.Telegram.WebApp
+
+  const onClose = () => {
+    tg.onClose()
+  }
 
   return (
     <div className="App">
-
+      <div onClick={onClose} C>close Window</div>
     </div>
   );
 }
