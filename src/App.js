9cb4ec1590252524bcd27d1ad4370b9d680e358b
Dmitry <brepex@21vek.it>
2023-01-30 16:05:41 +0700

    Merge remote-tracking branch 'origin/main'

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App container mx-auto px-2">
-      <div>close Window</div>
+      <div onClick={onClose}>close Window</div>
       <div>привет </div>
     </div>
   );
