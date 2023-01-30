cfb731fa96eafc9a664da3d96639d21fc4a3ef09
Dmitry <brepex@21vek.it>
2023-01-30 16:25:50 +0700

    Merge remote-tracking branch 'origin/main'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import {useTelegram} from "./hooks/useTelegram";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {Button, TextField} from "@mui/material";
 
 function App() {
 
     const {onClose, user, tg} = useTelegram()
+    const [task, setTask] = useState('')
+    const [tasks, setTasks] = useState([])
 
    useEffect(() => {
        tg.ready()
    } , [tg])
 
+    const handleTask = (e) => {
+        setTask(e.target.value)
+    }
+
+    const createTask = () => {
+        setTasks([...tasks, {task: task}])
+    }
+
+    useEffect(() => {
+        console.log(tasks)
+    }, [tasks])
+
   return (
     <div className="App container mx-auto px-2">
-      <div onClick={onClose}>close Window</div>
-      <div>привет {user}</div>
+      <div className={`text-2xl`}>Здравствуйте {user}</div>
+
+        <div className={`mt-4`}>
+            <TextField fullWidth={true} value={task} onChange={handleTask} label={'название задачи'}/>
+
+            <Button onClick={createTask}>Создать задачу</Button>
+        </div>
+
+        <div>
+            <div className={`text-xl`}>ваши задачи</div>
+            {tasks.map(tasking => <div className={`text-gray-400 text-md`}>{tasking.task}</div>)}
+        </div>
     </div>
   );
 }
