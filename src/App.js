1cfa2a4ee3521a28029385dcbda1c6d4040e2f95
Dmitry <brepex@21vek.it>
2023-01-30 11:06:25 +0700

    Merge remote-tracking branch 'origin/main'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import {useTelegram} from "./hooks/useTelegram";
+import {useEffect} from "react";
 
 function App() {
 
   const {onClose, user, tg} = useTelegram()
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
 
   return (
     <div className="App container mx-auto px-2">
